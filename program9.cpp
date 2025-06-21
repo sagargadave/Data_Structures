c3148c16a3495b25cfd090851198083faca8ea31
Sagar Gadave <sagargadave1234@gmail.com>
2025-06-21 22:39:59 +0530

    add DoublyLinked List

---
+++ program9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// node structure for Doubly Linked List
+struct node 
+{
+    int data;
+   struct node * prev;
+    struct node * next;
+};
+
+// Main function
+int main() 
+{
+    return 0;
+}
