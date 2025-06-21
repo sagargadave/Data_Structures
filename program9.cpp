db6f1d6d576836eaf877e28889be04662194a4de
Sagar Gadave <sagargadave1234@gmail.com>
2025-06-21 22:43:09 +0530

    add DoublyLinked List FirstInsert()

--- program9.cpp
+++ program9.cpp
@@ -5,12 +5,49 @@ using namespace std;
 struct node 
 {
     int data;
-   struct node * prev;
-    struct node * next;
+    struct node * prev;
+   struct node * next;
 };
+
+// Class for Doubly Linked List
+class DoublyLL 
+{
+private:
+    node* head;
+
+public:
+    DoublyLL();                    // Constructor
+    void InsertFirst(int no);     // Insert at the beginning
+};
+
+// Constructor
+DoublyLL::DoublyLL() 
+{
+    head = nullptr;
+}
+
+// Insert a node at the beginning
+void DoublyLL::InsertFirst(int no) 
+{
+    node* newNode = new node(no);
+
+    if (head != nullptr) 
+    {
+        newNode->next = head;
+        head->prev = newNode;
+    }
+
+    head = newNode;
+}
 
 // Main function
 int main() 
 {
+    DoublyLL dll;
+
+    dll.InsertFirst(30);
+    dll.InsertFirst(20);
+    dll.InsertFirst(10);
+
     return 0;
 }
